60e867695e51f5e2fe993f8bc4d485cf4188c17a
Florian ten Napel <tennapel.f@gmail.com>
2024-02-26 15:40:55 +0100

    Changed colorTheme to use hex values

--- src/main/java/no/uib/inf101/tetris/view/DefaultColorTheme.java
+++ src/main/java/no/uib/inf101/tetris/view/DefaultColorTheme.java
@@ -14,20 +14,20 @@ public class DefaultColorTheme implements ColorTheme {
   public DefaultColorTheme() {
     //TODO read colorMappings from file
     charToColorMap = new HashMap<>(Map.of(
-        '-', Color.DARK_GRAY,
+        '-', hexColor("8a8a8a"),
         'g', Color.GREEN,
         'y', Color.YELLOW,
         'r', Color.RED,
         'b', Color.BLUE,
-        'I', Color.CYAN,
-        'J', Color.BLUE,
-        'L', Color.ORANGE,
-        'O', Color.YELLOW,
-        'S', Color.GREEN.darker()
+        'I', hexColor("00bfc1"),
+        'J', hexColor("004ec1"),
+        'L', hexColor("ff6500"),
+        'O', hexColor("e2f427"),
+        'S', hexColor("0bdb3b")
     ));
     //Map.of has a max limit of 10 pairs
-    charToColorMap.put('T', Color.MAGENTA);
-    charToColorMap.put('Z', Color.RED);
+    charToColorMap.put('T', hexColor("ce28da"));
+    charToColorMap.put('Z', hexColor("d82a2a"));
 
     gameOverFont = new Font("Arial", Font.BOLD, 80);
     sideFont = new Font("Arial", Font.BOLD, 40);
@@ -70,5 +70,9 @@ public class DefaultColorTheme implements ColorTheme {
   @Override
   public Color getGameOverFontColor() {
     return Color.LIGHT_GRAY.brighter();
+  }
+
+  private Color hexColor(String hexValue) {
+    return new Color(Integer.parseInt(hexValue, 16));
   }
 }
--- src/main/java/no/uib/inf101/tetris/view/ShadowColorTheme.java
+++ src/main/java/no/uib/inf101/tetris/view/ShadowColorTheme.java
@@ -10,10 +10,6 @@ public class ShadowColorTheme extends DefaultColorTheme {
     float[] components = new float[4];
     components = original.getColorComponents(components);
 
-    for (int i = 0; i < 3; i++) {
-      components[i] = Math.min(1.0f, components[i] + 0.3f);
-    }
-
     return new Color(components[0], components[1], components[2], 0.6f);
   }
 }
